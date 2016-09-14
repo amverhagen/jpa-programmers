2eddab18a977e2b409fa419c2d87fd8cced38027
amverhagen <amverhagen93@gmail.com>
2016-09-13 21:18:51 -0400

    Initial commit.

---
+++ programmers/src/main/java/com/amverhagen/web/programmers/frontend/MyUI.java
@@ -0,0 +1,28 @@
+package com.amverhagen.web.programmers.frontend;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+@Theme("mytheme")
+public class MyUI extends UI {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		Label helloLabel = new Label("Hello Vaadin");
+		this.setContent(helloLabel);
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+		private static final long serialVersionUID = 1L;
+	}
+}
